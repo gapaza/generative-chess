f80e7c5d98ac4022dd33abe25b0237960a454fca
Ubuntu <ubuntu@ip-172-31-0-78.us-east-2.compute.internal>
2024-04-18 16:35:38 +0000

    updates for v2 model

--- model/__init__.py
+++ model/__init__.py
@@ -33,6 +33,37 @@ def get_rl_models(checkpoint_actor=None, checkpoint_critic=None):
 
 
 
+from model.ChessGPTv2 import ChessGPTv2
+
+
+def get_pretrain_model_v2(checkpoint_path=None):
+    model = ChessGPTv2()
+    test_input = tf.ones((1, config.seq_length))
+    piece_input = tf.ones((1, config.seq_length))
+    model([test_input, piece_input])
+    if checkpoint_path:
+        model.load_weights(checkpoint_path).expect_partial()
+    model.summary()
+    return model
+
+def get_rl_models_v2(checkpoint_actor=None, checkpoint_critic=None):
+    test_input = tf.ones((1, config.seq_length))
+    piece_input = tf.ones((1, config.seq_length))
+
+    actor_model = ChessGPTv2()
+    actor_model([test_input, piece_input])
+
+    critic_model = ChessGPTv2()
+    critic_model([test_input, piece_input])
+
+    if checkpoint_actor:
+        actor_model.load_weights(checkpoint_actor).expect_partial()
+    if checkpoint_critic:
+        critic_model.load_weights(checkpoint_critic).expect_partial()
+
+    actor_model.summary()
+    return actor_model, critic_model
+
 
 
 
---
+++ train_v2.py
@@ -0,0 +1,128 @@
+import config
+import platform
+import tensorflow as tf
+import tensorflow_addons as tfa
+import os
+from model import get_pretrain_model_v2 as get_model
+
+from preprocess.PTP_DatasetGenerator import PTP_DatasetGenerator
+
+
+curr_dataset = config.pt_dataset
+
+#
+#   _______           _         _
+#  |__   __|         (_)       (_)
+#     | | _ __  __ _  _  _ __   _  _ __    __ _
+#     | || '__|/ _` || || '_ \ | || '_ \  / _` |
+#     | || |  | (_| || || | | || || | | || (_| |
+#     |_||_|   \__,_||_||_| |_||_||_| |_| \__, |
+#                                          __/ |
+#                                         |___/
+#
+
+def train():
+
+
+
+    # 1. Build Model
+    checkpoint_path = config.model_path
+    model = get_model(checkpoint_path=checkpoint_path)
+
+    # 2. Get Optimizer
+    optimizer, jit_compile = get_optimizer()
+
+    # 3. Compile Model
+    model.compile(optimizer=optimizer, jit_compile=jit_compile)
+
+    # 4. Get Datasets
+    train_dataset, val_dataset = get_dataset()
+    # train_dataset = train_dataset.take(200)
+
+    # train_dataset = train_dataset.rebatch(128)
+    # val_dataset = val_dataset.rebatch(128)
+
+    # 5. Get Checkpoints
+    checkpoints = get_checkpoints()
+
+
+    # 6. Train Model
+    history = model.fit(
+        train_dataset,
+        epochs=config.epochs,
+        validation_data=val_dataset,
+        callbacks=checkpoints
+    )
+
+
+
+
+
+
+#
+#   _    _        _
+#  | |  | |      | |
+#  | |__| |  ___ | | _ __    ___  _ __  ___
+#  |  __  | / _ \| || '_ \  / _ \| '__|/ __|
+#  | |  | ||  __/| || |_) ||  __/| |   \__ \
+#  |_|  |_| \___||_|| .__/  \___||_|   |___/
+#                   | |
+#                   |_|
+#
+
+
+
+def get_dataset():
+
+    dataset_generator = PT_DatasetGenerator(curr_dataset)
+    train_dataset, val_dataset = dataset_generator.load_datasets()
+
+    return train_dataset, val_dataset
+
+
+def get_optimizer():
+    jit_compile = False
+    learning_rate = 0.0001
+    learning_rate = tf.keras.optimizers.schedules.CosineDecay(
+        0.0,
+        100000,
+        alpha=0.1,
+        warmup_target=learning_rate,
+        warmup_steps=500
+    )
+    optimizer = tfa.optimizers.RectifiedAdam(learning_rate=learning_rate)
+    if config.mixed_precision is True:
+        optimizer = tf.keras.mixed_precision.LossScaleOptimizer(optimizer)
+    return optimizer, jit_compile
+
+
+
+def get_checkpoints():
+    checkpoints = []
+    model_checkpoint = tf.keras.callbacks.ModelCheckpoint(
+        filepath=config.model_path,
+        save_weights_only=True,
+        monitor='val_loss',
+        mode='min',
+        save_best_only=True
+    )
+    checkpoints.append(model_checkpoint)
+    return checkpoints
+
+
+
+
+def train_distributed():
+    with config.mirrored_strategy.scope():
+        train()
+
+
+if __name__ == "__main__":
+    if config.distributed is False:
+        train()
+    else:
+        train_distributed()
+
+
+
+
